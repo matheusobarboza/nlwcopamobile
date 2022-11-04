d6812cbea37085d31725d54e4e8f0030cfc3f7ed
Matheus Barboza <matheus.barboza102@gmail.com>
2022-11-04 19:03:13 -0300

    feat: tela pools e find

--- src/screens/Find.tsx
+++ src/screens/Find.tsx
@@ -1,33 +1,29 @@
 import { Heading, VStack } from "native-base";
 import { Header } from "../components/Header";
 
-import Logo from '../assets/logo.svg';
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export const Find = () => {
   return (
     <VStack flex={1} bgColor="gray.900">
-      <Header 
-        title="Criar novo bolão"
-        showBackButton={true}
-      />
+      <Header title="Buscar por código" showBackButton />
 
       <VStack mt={8} mx={5} alignItems="center">
-        <Logo />
-
-        <Heading fontFamily="heading" color="white" fontSize="xl" my={8} textAlign="center">
-          Encontre o bolão através do seu código único
+        <Heading
+          fontFamily="heading"
+          color="white"
+          fontSize="xl"
+          mb={8}
+          textAlign="center"
+        >
+          Encontre um bolão através de {"\n"}
+          seu código único
         </Heading>
 
-        <Input 
-          mb={2}
-          placeholder="Qual o código do bolão?"
-        />
+        <Input mb={2} placeholder="Qual o código do bolão?" />
 
-        <Button 
-          title="BUSCAR BOLÃO"
-        />
+        <Button title="BUSCAR BOLÃO" />
       </VStack>
     </VStack>
   );
