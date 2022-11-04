2f28fad17c736aaf23bd549a1c0e26e2ec58cab3
Matheus Barboza <matheus.barboza102@gmail.com>
2022-11-04 20:56:31 -0300

    testando tela pool

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { NativeBaseProvider, StatusBar } from "native-base";
 
 import { Loading } from "./src/components/Loading";
 import { AuthContextProvider } from "./src/contexts/AuthContext";
-import { Pools } from "./src/screens/Pools";
+import { SignIn } from "./src/screens/SignIn";
 import { THEME } from "./src/styles/theme";
 
 export default function App() {
@@ -26,7 +26,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <SignIn /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
